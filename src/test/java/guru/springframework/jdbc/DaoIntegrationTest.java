5bace4f05e71601998265df56444c614f7625409
Vadim Piontkovskiy <vpiontkovs@wiley.com>
2022-03-22 15:35:25 +0300

    adding book dao impl

--- src/test/java/guru/springframework/jdbc/DaoIntegrationTest.java
+++ src/test/java/guru/springframework/jdbc/DaoIntegrationTest.java
@@ -2,8 +2,11 @@ package guru.springframework.jdbc;
 
 import guru.springframework.jdbc.dao.AuthorDao;
 
+import guru.springframework.jdbc.dao.BookDao;
 import guru.springframework.jdbc.domain.Author;
 
+import guru.springframework.jdbc.domain.Book;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -27,6 +30,70 @@ public class DaoIntegrationTest {
 
     @Autowired
     AuthorDao authorDao;
+    @Autowired
+    private BookDao bookDao;
+
+    @Test
+    void testGetBook() {
+        Book book = bookDao.getById(1L);
+
+        Assertions.assertThat(book).isNotNull();
+    }
+
+    @Test
+    void testFindBookByTitle() {
+        Book book = bookDao.findBookByTitle("Clean Code");
+
+        Assertions.assertThat(book).isNotNull();
+    }
+
+    @Test
+    void testSaveBook() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Self");
+        book.setTitle("my book");
+        book.setAuthorId(3L);
+//        Author author = new Author();
+//        author.setId(3L);
+//        book.setAuthor(author);
+
+        Book saved = bookDao.saveNewBook(book);
+        Assertions.assertThat(saved).isNotNull();
+    }
+
+    @Test
+    void testUpdateBook() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Self");
+        book.setTitle("my book");
+        book.setAuthorId(3L);
+
+//        Author author = new Author();
+//        author.setId(3L);
+//        book.setAuthor(author);
+
+        Book saved = bookDao.saveNewBook(book);
+
+        saved.setTitle("New Book");
+        Book fetched = bookDao.updateBook(saved);
+
+        Assertions.assertThat(fetched.getTitle()).isEqualTo("New Book");
+    }
+
+    @Test
+    void testDeleteBook() {
+        Book book = new Book();
+        book.setIsbn("1234");
+        book.setPublisher("Self");
+        book.setTitle("my book");
+        Book saved = bookDao.saveNewBook(book);
+
+        bookDao.deleteBookById(saved.getId());
+
+        assertThrows(EmptyResultDataAccessException.class, () -> bookDao.getById(saved.getId()));
+    }
 
     @Test
     void testDeleteAuthor() {
